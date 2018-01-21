c431560ee910965e8f3d566c5f1c386eb146e7b8
Dzmitry Zhyshkevich <dimach_96@mail.ru>
2018-01-21 22:28:47 +0300

    test

---
+++ 2201/test/assets/js/script.js
@@ -0,0 +1,13 @@
+let string = prompt ('введите строку');
+string = string.trim();
+alert ('длина строки:' + string.length);
+
+// let str = prompt ('введите строку');
+//
+// if (str != null && typeof str !== undefined) {
+//     str = str.trim();
+// }
+//
+// if (!str) {
+//     alert("Пусто");
+// }
