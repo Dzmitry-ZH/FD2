fadb8940a873c802c8d986c9c7adf02fa466591f
Dzmitry Zhyshkevich <dimach_96@mail.ru>
2018-01-24 09:02:52 +0300

    correction without sum

--- 2201a/2/assets/js/script.js
+++ 2201a/2/assets/js/script.js
@@ -1,17 +1,11 @@
 let number = prompt('Введите число');
 
+
 function countNumbers(number) {
-    let sum = 0;
-    let rest;
     if (!number) {
         return 0;
     }
-
-    rest = number % 10;
-    rest += countNumbers((number - rest) / 10);
-    sum += rest;
-    return sum;
+        return number%10 + countNumbers((number - number%10) / 10);
 }
-
 let x = countNumbers(number);
 alert(x);
